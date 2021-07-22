ff125b54dd29fea8415dfdcc7b7da7f13c6ee3bf
HOHO-00 <70706540+HOHO-00@users.noreply.github.com>
2021-07-22 23:55:55 +0800

    Add files via upload

---
+++ webcode/Unittest0/utils/common.py
@@ -0,0 +1,4 @@
+# 已经登录的cookie
+# cookie；一般默认30天失效 ； 或者有的15天失效（服务器设定的）
+
+cookie = [{'domain': '118.24.255.132', 'httpOnly': True, 'name': 'PHPSESSID', 'path': '/', 'secure': False, 'value': '52sk9nipis6c7kugdf8qgg6j87'}]
